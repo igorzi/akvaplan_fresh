896e240e5ab7f29cd53f64ad1064a0a0b3456576
cnrdh <cnrdh@users.noreply.github.com>
2023-05-15 14:16:33 +0200

    start work-with-us

--- routes/careers.tsx
+++ routes/careers.tsx
@@ -1,1 +1,44 @@
 ///no/news?q=stillingsannonse|
+///en/work-with-us
+import { Page } from "akvaplan_fresh/components/page.tsx";
+import { lang, t } from "akvaplan_fresh/text/mod.ts";
+
+import {
+  type HandlerContext,
+  type Handlers,
+  type PageProps,
+  type RouteConfig,
+} from "$fresh/server.ts";
+
+interface NullProps {
+  lang: string;
+  base: string;
+  title: string;
+}
+
+export const config: RouteConfig = {
+  routeOverride:
+    "/:lang(en|no)/:page(work-with-us|career|jobb-hos-oss|karriere)",
+};
+
+export const handler: Handlers = {
+  GET(req: Request, ctx: HandlerContext) {
+    const { params } = ctx;
+    lang.value = params.lang;
+    const title = t(params.page);
+    const base = `/${params.lang}/${params.page}/`;
+    return ctx.render({ lang, title, base });
+  },
+};
+
+export default function Null(
+  { data: { title, lang, base } }: PageProps<NullProps>,
+) {
+  return (
+    <Page title={title} base={base}>
+      <h1>
+        <a href=".">{title}</a> [{t(`lang.${lang}`)}]
+      </h1>
+    </Page>
+  );
+}
