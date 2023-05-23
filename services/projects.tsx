303783361e0f1e29a5ba70807fe8cbc196549679
cnrdh <cnrdh@users.noreply.github.com>
2023-05-23 14:19:43 +0200

    add project service

---
+++ services/projects.tsx
@@ -0,0 +1,17 @@
+// https://akvaplan.no/en/news/2016-01-26/the-norwegian-ministry-of-foreign-affair-supports-akvaplan-niva-project-by-nok-14.3-million",
+// VEIEN http://localhost:7777/en/news/2023-01-30/hvordan-kan-rognkjeks-bli-en-robust-og-effektiv-rensefisk-i-lakseoppdrett
+
+const polarfront = {
+  logo:
+    "https://resources.mynewsdesk.com/image/upload/f_auto,t_limit_1000/fptidcnhyeuhbohaggpx.jpg",
+  searchwords: ["PolarFront", "polar-front-ecology"],
+};
+const criptic = {
+  logo:
+    "https://resources.mynewsdesk.com/image/upload/dpr_auto,f_auto,q_auto:good/icba6p15vg8yhelepmkb",
+};
+
+export const projectMap = new Map([
+  ["polarfront", polarfront],
+  ["criptic, criptic"],
+]);
