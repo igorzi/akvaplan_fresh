215eadad7c833b152ed7646a1f54946da4049118
cnrdh <cnrdh@users.noreply.github.com>
2023-05-24 10:51:54 +0200

    fix criptic logo

--- services/projects.tsx
+++ services/projects.tsx
@@ -2,16 +2,19 @@
 // VEIEN http://localhost:7777/en/news/2023-01-30/hvordan-kan-rognkjeks-bli-en-robust-og-effektiv-rensefisk-i-lakseoppdrett
 
 const polarfront = {
+  mynewsdesk: {
+    id: 104226,
+  },
   logo:
     "https://resources.mynewsdesk.com/image/upload/f_auto,t_limit_1000/fptidcnhyeuhbohaggpx.jpg",
   searchwords: ["PolarFront", "polar-front-ecology"],
 };
 const criptic = {
   logo:
-    "https://resources.mynewsdesk.com/image/upload/dpr_auto,f_auto,q_auto:good/icba6p15vg8yhelepmkb",
+    "https://resources.mynewsdesk.com/image/upload/f_auto,t_limit_1000/icba6p15vg8yhelepmkb.jpg",
 };
 
 export const projectMap = new Map([
   ["polarfront", polarfront],
-  ["criptic, criptic"],
+  ["criptic", criptic],
 ]);
