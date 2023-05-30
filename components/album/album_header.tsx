39b905108ff719e0d8af35bc0da3b024e3400c2b
cnrdh <cnrdh@users.noreply.github.com>
2023-05-30 14:14:13 +0200

    reduce font-size for AlbumHeader (and temporarily remove Icon)

--- components/album/album_header.tsx
+++ components/album/album_header.tsx
@@ -1,15 +1,12 @@
 import { Icon } from "akvaplan_fresh/components/icon.tsx";
 
-// console.log(
-//   "@todo AlbumHeader: margin before as function of viewport ie. media query (3rem nice in XXL only)",
-// );
 export const AlbumHeader = ({ text, href, icon = "arrow_forward_ios" }) => (
-  <h1 style={{ marginBlockStart: "0.25rem" }}>
+  <span style={{ marginBlockStart: "0.25rem", fontSize: "var(--font-size-4)" }}>
     <a
       href={href}
       style={{ color: "var(--text1)" }}
     >
-      {text} <Icon name={icon} />
+      {text} {/* <Icon name={icon} /> */}
     </a>
-  </h1>
+  </span>
 );
