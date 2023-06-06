ae795612117710bbf98d904419011d76fdbb055c
Petter Kaspersen <petter@kaspersen.dev>
2023-06-06 12:53:58 +0200

    Header with left aligned icon

--- components/header/clean_header.tsx
+++ components/header/clean_header.tsx
@@ -1,28 +1,24 @@
 import { ApnLogo } from "akvaplan_fresh/components/mod.ts";
 import { lang as langSignal, t } from "akvaplan_fresh/text/mod.ts";
-import borders from "open-props/src/props.borders.js";
 import { SiteMenu } from "./site_menu.tsx";
-export function CleanHeader(
-  {
-    href = "/",
-    lang = langSignal.value,
-    Logo = ApnLogo,
-    Right = SiteMenu,
-    Left = () => <div />,
-  },
-) {
+
+export function CleanHeader({
+  href = "/",
+  lang = langSignal.value,
+  Logo = ApnLogo,
+  Right = SiteMenu,
+}) {
   return (
     <header
       id="top"
       style={{
         margin: 0,
-        padding: borders["--border-size-3"],
-        display: "grid",
-        gridTemplateColumns: "1fr auto 1fr",
+        padding: "var(--border-size-4)",
+        display: "flex",
+        justifyContent: "space-between",
         color: "var(--text1)",
       }}
     >
-      <Left lang={lang} />
       <a href={href}>
         <Logo />
       </a>
