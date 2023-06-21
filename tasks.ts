2f024782ad01e470afbea7130ed016d608b39cd7
cnrdh <cnrdh@users.noreply.github.com>
2023-06-21 15:06:16 +0200

    refactor icons task with new menu icon

--- tasks.ts
+++ tasks.ts
@@ -1,36 +1,43 @@
 import {
   fetchIcon,
-  icons,
+  icons as materialsymbols,
   processIcon,
+  svgMapFromStaticDir,
 } from "akvaplan_fresh/utils/materialsymbols/mod.ts";
 
-const iconPath = (name: string) => `static/icon/${name}.svg`;
+const iconDir = "static/icon";
 
-// const fetchAndSaveIcons = async (_args: string[]) => {
-//   for (const name of icons) {
-//     const text = await fetchIcon(name);
-//     const path = iconPath(name);
-//     Deno.writeTextFile(path, processIcon(text, name));
-//     console.log({ path });
-//   }
-// };
+const iconPath = (name: string) => `${iconDir}/${name}.svg`;
 
-const hamburger_menu_right =
-  `<svg id="hamburger_menu_right" viewBox="0 0 24 24" fill="currentColor"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 5C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H4ZM7 12C7 11.4477 7.44772 11 8 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H8C7.44772 13 7 12.5523 7 12ZM13 18C13 17.4477 13.4477 17 14 17H20C20.5523 17 21 17.4477 21 18C21 18.5523 20.5523 19 20 19H14C13.4477 19 13 18.5523 13 18Z" /></svg>`;
-
-const fetchAndSaveIconTSX = async (_args: string[]) => {
-  const entries = [["hamburger_menu_right", hamburger_menu_right]];
-  for (const name of icons) {
-    const _text = await fetchIcon(name);
-    const text = processIcon(_text, name);
-    entries.push([name, text]);
+const fetchAndSaveMaterialSymbolIcons = async (list: Set<string>) => {
+  for (const name of list) {
+    const text = await fetchIcon(name);
+    const path = iconPath(name);
+    Deno.writeTextFile(path, processIcon(text, name));
+    console.log({ path });
   }
-  Deno.writeTextFile("components/icons.json", JSON.stringify(entries));
+};
+
+const taskIcons = async (_args: string[] = []) => {
+  await fetchAndSaveMaterialSymbolIcons(materialsymbols);
+
+  const hamburger_menu_right = await Deno.readTextFile(
+    new URL(import.meta.resolve("./static/icon/hamburger_menu_right.svg")),
+  );
+  const map = await svgMapFromStaticDir();
+  map.set("hamburger_menu_right", hamburger_menu_right);
+  // for (const name of materialsymbols) {
+  //   const _svg = await Deno.readTextFile(
+  //     new URL(import.meta.resolve(`./static/icon/${name}.svg`)),
+  //   );
+  //   const svg = processIcon(_svg, name);
+  //   entries.push([name, svg]);
+  // }
+  Deno.writeTextFile("components/icons.json", JSON.stringify([...map]));
 };
 
 const tasks = new Map([
-  //["icons-save", fetchAndSaveIcons], // Add icons (to /static/icon/*.svg) by expanding the `icons` set and run "deno task icons"
-  ["icons", fetchAndSaveIconTSX],
+  ["icons", taskIcons],
 ]);
 
 if (import.meta.main) {
