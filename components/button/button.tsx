80ff8b7e12ff4ac2e6487431f6cbaa46cb6a3ca6
Petter Kaspersen <petter@kaspersen.dev>
2023-06-23 10:25:08 +0200

    Add support for filled button

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -2,11 +2,22 @@ import { HTMLProps } from "preact";
 
 type Props = HTMLProps<HTMLButtonElement> & {
   additionalClass?: string;
+  filled?: boolean;
 };
 
-export default function Button({ additionalClass = "", children, ...props }: Props) {
+export default function Button({
+  additionalClass = "",
+  filled = false,
+  children,
+  ...props
+}: Props) {
   return (
-    <button class={`custom-button ${additionalClass}`} {...props}>
+    <button
+      class={`custom-button ${additionalClass} ${
+        filled ? "custom-button-filled" : ""
+      }`}
+      {...props}
+    >
       {children}
     </button>
   );
