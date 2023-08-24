619a2f37ffb4a9e3edc5fd8d052f418c8d329879
cnrdh <cnrdh@users.noreply.github.com>
2023-08-24 09:41:19 +0200

    clean

--- routes/null.tsx
+++ routes/null.tsx
@@ -1,17 +1,16 @@
 import { Page } from "akvaplan_fresh/components/page.tsx";
 import { t } from "akvaplan_fresh/text/mod.ts";
-export {
-  handler,
-  type InternationalProps,
+import type {
+  InternationalProps,
 } from "akvaplan_fresh/utils/page/international_page.ts";
 
 import { PageProps, RouteConfig } from "$fresh/server.ts";
 export const config: RouteConfig = {
   routeOverride: "/:lang(en|no)/:page(null|0)",
 };
-export default function Documents(
-  { data: { title, lang, base } }: PageProps<InternationalProps>,
-) {
+
+export default function N0ll(props: InternationalProps) {
+  const { title, lang, base } = props;
   return (
     <Page title={title} base={base}>
       <h1>
