15fbefc03d7a1f46a9b4ea0d2cb47ae99141eda2
cnrdh <cnrdh@users.noreply.github.com>
2023-09-29 12:09:45 +0200

    use img

---
+++ components/alt_lang.tsx
@@ -0,0 +1,36 @@
+import { t } from "akvaplan_fresh/text/mod.ts";
+
+const _caption = {
+  fontSize: "0.75rem",
+};
+export const OnlyIn = ({ language, lang }) => {
+  return <div lang={lang}>{t(`lang.Only.${String(language)}`)}</div>;
+};
+export const AlsoInNative = ({ href, hreflang, lang }) => {
+  return (
+    <span lang={lang}>
+      {t(`lang.Also_in_native.${String(hreflang)}`)}
+      {": "}
+      <a hreflang={hreflang} href={href}>{href}</a>
+    </span>
+  );
+};
+// lang = site lang
+// language = current document's language
+// alternate = link to alternate version
+export const AltLangInfo = ({ alternate, lang, language }) => (
+  <section style={_caption}>
+    <em style={{ color: "var(--text2)" }}>
+      {alternate && lang !== language
+        ? (
+          <AlsoInNative
+            href={alternate.href}
+            hreflang={alternate.hreflang}
+            lang={alternate.hreflang}
+          />
+        )
+        : null}
+      {!alternate && lang !== language && OnlyIn({ lang, language })}
+    </em>
+  </section>
+);
